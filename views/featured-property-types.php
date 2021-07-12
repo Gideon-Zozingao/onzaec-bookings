34d63a612e76416282ede0483c0b1890c59e0788
Gideon-Zozingao <gideonzozingao@switchmaven.com>
2021-07-13 04:03:09 +1000

    Some more Change sfor this File

--- views/featured-property-types.php
+++ views/featured-property-types.php
@@ -22,17 +22,26 @@ try {
   GROUP BY  propertyType");
   $query->setFetchMode(PDO::FETCH_ASSOC);
   $rowCount=$query->countRow();
-  while($row=$query->fetch()){?>
-    <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
-      <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
-        <h4 class="title"><a href=""><?php echo $row["propertyType"];  ?></a></h4>
-        <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
+  if($rowCount>0){
+    while($row=$query->fetch()){?>
+      <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+        <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
+          <h4 class="title"><a href=""><?php echo $row["propertyType"];  ?></a></h4>
+          <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
+        </div>
       </div>
+
+      <?php
+
+    }
+  }else{
+    ?>
+    <div class="container">
+      <h5 class="text-center text-muted">Information for this section is not availabale at this time</h5>
     </div>
-
     <?php
-
   }
+
 
 } catch (PDOException $e) {
 echo $e->getMessage();
